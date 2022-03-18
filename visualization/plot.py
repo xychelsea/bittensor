f80712ece136f37e788adaf86d120bc1fa1b3359
isabella618033 <isabella618033@gmail.com>
2022-03-18 13:18:32 -0400

    place holder

--- visualization/plot.py
+++ visualization/plot.py
@@ -25,7 +25,9 @@ app.layout = html.Div(children=[
 
     html.Div(children='~~~ Respond time check ~~~'),
 
-    dcc.Graph(id="respond-time"),
+    # dcc.Graph(id="respond-time"),
+    dcc.Graph(id="dummy-respond-time-line"),
+    dcc.Graph(id="dummy-respond-time-hist"),
     dcc.Checklist(
         id="respond-time-filter",
         options=[10, 110],
@@ -40,19 +42,37 @@ app.layout = html.Div(children=[
 
 ])
 
-@app.callback(Output("respond-time", "figure"), Input("respond-time-filter", "value"))
-def respond_time_plot(sequence_lens):
-    results = pd.read_csv('/home/isabella/.bittensor/bittensor/visualization/results.csv')
-    results = results.sort_values(by =['time'])
-    results = results.reset_index(drop = True)
-    mask = results.sequence_len.isin(sequence_lens)
-    results_masked = results[mask].reset_index()
-    # results_masked['index'] = results_masked['index'] % 2000
+# @app.callback(Output("respond-time", "figure"), Input("respond-time-filter", "value"))
+# def respond_time_plot(sequence_lens):
+#     results = pd.read_csv('/home/isabella/.bittensor/bittensor/visualization/data/results.csv')
+#     results = results.sort_values(by =['time'])
+#     results = results.reset_index(drop = True)
+#     mask = results.sequence_len.isin(sequence_lens)
+#     results_masked = results[mask].reset_index()
+#     # results_masked['index'] = results_masked['index'] % 2000
 
-    print(results_masked)
-    fig = px.line(results_masked, x = 'index', y="time", color='sequence_len', title="Sorted respond time")
+#     fig = px.line(results_masked, x = 'index', y="time", color='sequence_len', title="Sorted respond time")
+#     return fig
+
+
+@app.callback(Output("dummy-respond-time-line", "figure"), Input("respond-time-filter", "value"))
+def dummy_respond_time_line(sequence_lens):
+    result = pd.read_json('/home/isabella/.bittensor/bittensor/visualization/exp_results/mock_result.txt')
+    result = result.transpose()
+    print(result)
+    result = result.sort_values(by =['time'])
+    result = result.reset_index(drop = True)
+    fig = px.line(result, y="time", title="Dummy respond time")
     return fig
 
+@app.callback(Output("dummy-respond-time-hist", "figure"), Input("respond-time-filter", "value"))
+def dummy_respond_time_hist(sequence_lens):
+    result = pd.read_json('/home/isabella/.bittensor/bittensor/visualization/exp_results/mock_result.txt')
+    result = result.transpose()
+    result = result.astype({'code': 'str'})
+    
+    fig = px.histogram(result, x="code", title="Dummy respond code")
+    return fig
 
 if __name__ == '__main__':
     app.run_server(debug=True)
