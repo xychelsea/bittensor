a47cb2ecc9c32eee813fdb8644fc2e6fe200efc3
isabella618033 <isabella618033@gmail.com>
2022-04-25 22:01:14 -0400

    .

--- bittensor/_neuron/text/exp_validator/__init__.py
+++ bittensor/_neuron/text/exp_validator/__init__.py
@@ -694,8 +694,8 @@ class nucleus( torch.nn.Module ):
         # onto the targets.
         # target_loss: (torch.float64): loss after decoding all responses and a variance loss.
         # target_loss.shape = [ 1 ]
+        responses_hidden, _ = joining_context( return_ops, batchwise_routing_weights[routing_index], query_responses)  
         if self.config.nucleus.join_logits == False:
-            responses_hidden, _ = joining_context( return_ops, batchwise_routing_weights[routing_index], query_responses)  
             target_loss = self.get_target_loss ( responses_hidden, inputs )
 
         else:
