845292d154e34c6bc89b38a1b4101121a7d105b0
isabella618033 <isabella618033@gmail.com>
2022-05-03 19:33:19 -0400

    fix

--- bittensor/_neuron/text/exp_validator/__init__.py
+++ bittensor/_neuron/text/exp_validator/__init__.py
@@ -565,8 +565,8 @@ class nucleus( torch.nn.Module ):
         src_mask = torch.triu(torch.ones(hidden.size(1), hidden.size(1)) * float('-inf'), diagonal=1)
         src_mask = src_mask.to(self.config.neuron.device)
         encoded_hidden = self.encoder( hidden, mask = src_mask )
-        decoder_gate_score = self.decoder_gate(encoded_hidden)
-        sub_hiddens = [score * sub_decoder(encoded_hidden) for score, sub_decoder in zip(decoder_gate_score, self.sub_decoder)]
+        decoder_gate_score = torch.mean(torch.mean(self.decoder_gate(encoded_hidden), axis = 0), axis = 0)
+        sub_hiddens = [score * sub_decoder(encoded_hidden) for score, sub_decoder in zip(decoder_gate_score.tolist(), self.sub_decoder)]
         decoded_targets = self.decoder( sum(sub_hiddens) )
         shift_logits = decoded_targets[..., :-1, :].contiguous()
         return shift_logits
