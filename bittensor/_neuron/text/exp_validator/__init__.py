d21f1c8e2219a752089d37d8209af4cfb3f353b3
isabella618033 <isabella618033@gmail.com>
2022-05-06 16:39:58 -0400

    gate init with uniform

--- bittensor/_neuron/text/exp_validator/__init__.py
+++ bittensor/_neuron/text/exp_validator/__init__.py
@@ -560,7 +560,7 @@ class nucleus( torch.nn.Module ):
         # === Resets all the weights using xavier initialization. ===
         torch.nn.init.xavier_uniform_ ( self.token_embedding.weight )
         torch.nn.init.xavier_uniform_ ( self.decoder.weight )
-        torch.nn.init.normal_ ( self.gates, std = 1/(13 + self.num_others) )
+        torch.nn.init.xavier_uniform ( self.gates, 1/(13 + self.num_others) )
         def init_xavier( component ):
             try:
                 torch.nn.init.xavier_uniform_( component.weight )
