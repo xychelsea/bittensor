9e06a92ea7fd8b981fea657193de26f8b8159ebf
isabella618033 <isabella618033@gmail.com>
2022-05-17 14:54:40 -0400

    removed topk

--- bittensor/_neuron/text/exp_validator/__init__.py
+++ bittensor/_neuron/text/exp_validator/__init__.py
@@ -591,12 +591,12 @@ class nucleus( torch.nn.Module ):
         
         decoder_gate_score = torch.mean(torch.mean(self.decoder_gate(hidden), axis = 0), axis = 0)
         # self.penalty += self.decoder_gate_penalty(decoder_gate_score, torch.zeros_like(decoder_gate_score))
-        k = max(40 - round(self.global_step/2), 5)
-        k = min (k, self.num_sub_decoder)
-        idx = decoder_gate_score.topk(k, largest = False)[1]
-        decoder_gate_score[idx] = -10
+        # k = max(40 - round(self.global_step/2), 5)
+        # k = min (k, self.num_sub_decoder)
+        # idx = decoder_gate_score.topk(k, largest = False)[1]
+        # decoder_gate_score[idx] = -10
         decoder_gate_score = nn.functional.softmax(decoder_gate_score , dim = 0)
-        sub_hiddens = [score * sub_decoder(hidden) for score, sub_decoder in zip(decoder_gate_score.tolist(), self.sub_decoder) if score > 0.001]
+        sub_hiddens = [score * sub_decoder(hidden) for score, sub_decoder in zip(decoder_gate_score.tolist(), self.sub_decoder) if score > 0.05]
         encoded_hidden = self.encoder( sum(sub_hiddens), mask = src_mask )
         decoded_targets = self.decoder( encoded_hidden )
         shift_logits = decoded_targets[..., :-1, :].contiguous()
